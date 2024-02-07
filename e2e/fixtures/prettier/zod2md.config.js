07b0ca37aee8a52400d04d4348e03586371a1c61
Matěj Chalk <matej.chalk@flowup.cz>
2024-02-07 20:28:31 +0100

    Fix cannot find *.cjs bundle-require errors

--- e2e/fixtures/prettier/zod2md.config.js
+++ e2e/fixtures/prettier/zod2md.config.js
@@ -1,5 +1,6 @@
 export default {
   title: 'Prettier configuration file reference',
   entry: 'prettierrc.js',
+  format: 'esm',
   output: '../../../tmp/config/prettier.md',
 };
--- src/cli/import-config.ts
+++ src/cli/import-config.ts
@@ -2,7 +2,7 @@ import { bundleRequire } from 'bundle-require';
 import type { Config } from '../types';
 
 export async function importConfig(path: string): Promise<Config> {
-  const { mod } = await bundleRequire({ filepath: path });
+  const { mod } = await bundleRequire({ filepath: path, format: 'esm' });
   // TODO: validation
   return mod.default || mod;
 }
