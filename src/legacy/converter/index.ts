e8ddeee9d85ce70d608b93987073f5ec2ab11ca1
Matěj Chalk <matej.chalk@flowup.cz>
2025-11-06 23:33:25 +0100

    Replace legacy implementation

--- src/legacy/converter/index.ts
+++
@@ -1,1 +0,0 @@
-export { convertSchemas } from './convert-schemas';
--- src/legacy/formatter/types.ts
+++
@@ -1,9 +0,0 @@
-export type NameTransformFn = (
-  name: string | undefined,
-  path: string
-) => string;
-
-export type FormatterOptions = {
-  title: string;
-  transformName?: NameTransformFn;
-};
